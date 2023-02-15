4a74fe09241017216c89a565d6bf4e2b5d014702
Jabir <220501@student.glu.nl>
2023-02-15 16:17:16 +0100

    commit

--- handlers/login.php
+++ handlers/login.php
@@ -10,16 +10,25 @@ if (
 
     $sqlA = "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password' AND `usertype`='admin'";
     $resultA = mysqli_query($conn, $sqlA);
+   
 
     $sql = "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password'";
     $result = mysqli_query($conn, $sql);
-    
-    $name = "SELECT `id` FROM `users` WHERE `name`";
 
+    $row = mysqli_fetch_assoc($result);
+    $name = $row['name'];
+  
     if (mysqli_num_rows($resultA) > 0) {
         header('location: ../auth/admin/dashboard.php/');
+        session_start();
+        $_SESSION["signedIn"] = true;
+        $_SESSION["name"] = $name;
+
     } elseif (mysqli_num_rows($result) > 0) {
         header('location: ../auth/dashboard/home.php');
+        session_start();
+        $_SESSION["signedIn"] = true;
+        $_SESSION["name"] = $name;
     } else {
         header("location: ../auth/login?msg=Error: Email and password does not match, try again.");
     }
--- includes/movie_sliders/upcoming_movies.php
+++ includes/movie_sliders/upcoming_movies.php
@@ -22,10 +22,12 @@ include './database/connection.php'
 
                 ?>
                 <div class="movie_item">
-                    <img src="<?php echo $img.$movie_poster?>" alt="">
+                    <img src="<?php echo $img . $movie_poster ?>" alt="">
                     <div class="info_film">
                         <div class="genre">
-                            <span><?php echo $movie_genre?></span>
+                            <span>
+                                <?php echo $movie_genre ?>
+                            </span>
                         </div>
                         <div id="rate">
                             <?php
@@ -34,11 +36,11 @@ include './database/connection.php'
                             <path d="M6.99998 2.61358L7.95838 5.36525L8.13368 5.86856H8.66665H11.5206L9.3106 7.62811L8.91738 7.9412L9.05748 8.42391L9.86844 11.2179L7.41982 9.56375L6.99998 9.28012L6.58014 9.56375L4.13152 11.2179L4.94248 8.42391L5.08258 7.9412L4.68936 7.62811L2.47939 5.86856H5.33331H5.86628L6.04158 5.36525L6.99998 2.61358Z" fill="white" stroke="white" stroke-width="1.5"/>
                             </svg>';
                             }
-            }
-        }
-        ?>
-                </div>
-                <h3><?php echo $movie_name ?></h3>
+                            ;
+                            </div>
+                <h3>
+                    <?php echo $movie_name ?>
+                </h3>
                 <div class="hover_watch">
                     <span class="movie_type"><a href="#">Watch Now</a></span>
                     <span>&#10095;</span>
@@ -46,5 +48,9 @@ include './database/connection.php'
             </div>
             <div class="overlay-film_scroll"></div>
         </div>
+            }
+        }
+        ?>
+                
     </div>
 </div>
